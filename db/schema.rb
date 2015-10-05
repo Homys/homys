4df19f7154ea79c17cadb9f9dd1048f71bfb7da6
Jill Cates <cates.jill@gmail.com>
2015-10-05 18:10:13 -0400

    Removed user_id from Expense model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005220419) do
+ActiveRecord::Schema.define(version: 20151005220919) do
 
   create_table "announcements", force: :cascade do |t|
     t.string   "title"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20151005220419) do
     t.string   "title"
     t.float    "amount"
     t.datetime "date_due"
-    t.integer  "user_id"
     t.integer  "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
