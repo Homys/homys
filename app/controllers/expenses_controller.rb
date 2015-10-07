ee15d4858b38b503bb235fbe7dfa3bf8e742b43a
Cobi Moos <cobimoos100@gmail.com>
2015-10-07 13:02:19 -0400

    adjusted expense page so owner of expense can perform certain actions, added a delete expense, and added placeholder links for now to be filled later with AJAX

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -8,6 +8,9 @@ class ExpensesController < ApplicationController
   end
 
   def create
+    #first checks to see if expense row has been updated.
+    #Due to the 'has_and_belongs_to_many' relationhip with users,
+    #the method then checks to see if it can update the expenses_users table
     @expense = Expense.new(expense_params)
     @expense.owner_id = current_user.id
 
@@ -23,13 +26,17 @@ class ExpensesController < ApplicationController
   end
 
   def destroy
+    expense = Expense.find(params[:id])
+    expense.destroy
   end
 
   def update
     @expense = Expense.find(params[:id])
 
+    #first checks to see if expense row has been updated.
+    #Due to the 'has_and_belongs_to_many relationhip' with users,
+    #the method then checks to see if it can update the expenses_users table
     @expense.update_attributes(expense_params)
-
     redirect_to expense_path(@expense)
 
   end
