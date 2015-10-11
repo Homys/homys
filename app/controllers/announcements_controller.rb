67bb2feaebab2009474240781cd1c4e1410c29ed
Jill Cates <cates.jill@gmail.com>
2015-10-10 22:44:12 -0700

    Bug fixed in announcements controller

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -28,7 +28,8 @@ class AnnouncementsController < ApplicationController
 		end
 
 		if @announcements.length == 0
-      flash[:alert] = "You have no announcements. Create one now to get started."
+      		flash[:alert] = "You have no announcements. Create one now to get started."
+		end 
 	end
 
 	def destroy
