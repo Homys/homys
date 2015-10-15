f3024cfbf138c7f1008e7bc044e42be7c09f0366
Jill Cates <cates.jill@gmail.com>
2015-10-15 18:56:29 -0400

    NavBar style modified

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -16,13 +16,15 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   # end
 
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
+  def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "robohash.png"].compact.join('_'))
+  # "https://robohash.org/#{current_user.first_name}"
+  # {}"/images/fallback/" + [version_name, "robohash.png"].compact.join('_')
+  end
+
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
