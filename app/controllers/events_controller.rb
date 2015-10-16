85c4585d8e5408f835c9c63ff74becc9d2efb17c
Jill Cates <cates.jill@gmail.com>
2015-10-16 12:31:29 -0400

    Event page almost ajaxified

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,12 +6,23 @@ class EventsController < ApplicationController
 
 	def create
 		@event = Event.new(event_params)
-		@event.owner_id = current_user.id
+		@event.house = @house 
+		@event.owner = current_user
 
 		if @event.save
 			redirect_to house_events_path(current_user.house)
 		else
 			render :new
+		end
+
+		respond_to do |format|
+			if @event.save
+				format.html { redirect_to house_events_path(current_user.house), notice: 'Event created.' }
+	      format.js {}
+			else
+				format.html { render :index, alert: 'There was an error.'  }
+	      format.js {render status: '422'}
+			end
 		end
 	end
 
