d78a6e8300ee2569ab5df369cfd47bce5599c711
Jill Cates <cates.jill@gmail.com>
2015-10-16 00:45:51 -0400

    Card style added to Houses_show view

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -20,7 +20,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "robohash.png"].compact.join('_'))
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "robohash.jpeg"].compact.join('_'))
   
     # Faker::Avatar.image("#{current_user}", "50x50", "bmp")
   # {}"/images/fallback/" + [version_name, "robohash.png"].compact.join('_')
