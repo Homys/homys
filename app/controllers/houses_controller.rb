03c3134c56884ac0a2bde7baf348939967c20727
Cobi Moos <cobimoos100@gmail.com>
2015-10-25 16:38:21 -0400

    stylized create house page

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -57,9 +57,11 @@ class HousesController < ApplicationController
   def index
     if current_user.house_id != nil
       redirect_to house_path(current_user.house)
-    end 
+    else
+      @house = House.new
+    end
 
-  end 
+  end
 
 
   private
