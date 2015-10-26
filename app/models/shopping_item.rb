d5a35ecbf68383c895e13a0e45705a84023759be
Cobi Moos <cobimoos100@gmail.com>
2015-10-26 14:14:45 -0400

    made contents of card mobile friendly

--- app/models/shopping_item.rb
+++ app/models/shopping_item.rb
@@ -2,12 +2,12 @@ class ShoppingItem < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
   belongs_to :user
   belongs_to :house
-  validates_length_of :name, :maximum => 60, :allow_blank => false
+  validates_length_of :name, :maximum => 40, :allow_blank => false
 
   def reduce_points
   	if points_rewarded > 100
   	update_attribute(:points_rewarded, (self.points_rewarded - 100))
   end
-  end 
+  end
 
 end
