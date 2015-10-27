8646f03144b8255028d4b0c74720a71a2a562bf8
Jill Cates <cates.jill@gmail.com>
2015-10-27 16:47:23 -0400

    Logic for unique phone numbers implemented

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -5,11 +5,17 @@ class PhoneNumbersController < ApplicationController
 	end
 
 	def create
-  	@phone_number = PhoneNumber.find_or_initialize_by(phone_number: params[:phone_number][:phone_number])
-  	@phone_number.user = current_user
-    @phone_number.save
-    @phone_number.generate_pin
-  	@phone_number.send_pin
+  	
+    if PhoneNumber.find_by_phone_number(params[:phone_number][:phone_number])
+      @phone_number = PhoneNumber.new
+    else
+      @phone_number = PhoneNumber.new(phone_number: params[:phone_number][:phone_number])
+      @phone_number.user = current_user
+
+      @phone_number.save
+      @phone_number.generate_pin
+    	@phone_number.send_pin
+    end
 
   	respond_to do |format|
     	format.js # render app/views/phone_numbers/create.js.erb
@@ -18,7 +24,12 @@ class PhoneNumbersController < ApplicationController
 
 	def verify
     @phone_number = PhoneNumber.find_by(phone_number: params[:hidden_phone_number])
+    unless @phone_number.user == current_user
+      render :new, notice: "Hey, that's not your phone number!"
+    end
+
     @phone_number.verify(params[:pin])
+
     current_user.phone_num = @phone_number.phone_number 
     current_user.is_verified = true
     current_user.save
